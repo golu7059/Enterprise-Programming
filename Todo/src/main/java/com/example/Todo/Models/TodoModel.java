4d92ae354a0eda1fb630eb525806dbe78aeda2a7
golu Kumar <golukumarjaisidih@gmail.com>
2024-10-22 22:21:46 +0530

    initialized todo app in java

---
+++ Todo/src/main/java/com/example/Todo/Models/TodoModel.java
@@ -0,0 +1,30 @@
+public class TodoModel {
+    private int id;
+    private String content;
+
+    public TodoModel(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    pubilc TodoModel() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
